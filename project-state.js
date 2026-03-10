91e1aff6247a24164128265f08a03dedbf9e2a16
Mathew <mathewmit@gmail.com>
2026-03-10 13:10:09 -0400

    Initial UKTripCodex workspace

---
+++ project-state.js
@@ -0,0 +1,209 @@
+window.UK_TRIP_PROJECT_STATE = {
+  "photoOverrides": {
+    "303_E_83rd_St_NYC": {
+      "left": "303 E. 83rd lobby.webp",
+      "leftCarousel": [
+        "nyc-apt.webp",
+        "303 E. 83rd lobby.webp"
+      ]
+    },
+    "South_Bank": {
+      "leftBottom": "south-bank.jpg",
+      "rightTop": "south-bank-3.jpg",
+      "rightBottom": "south-bank-2.jpg",
+      "leftCarousel": [
+        "south-bank.png",
+        "south-bank.jpg"
+      ]
+    },
+    "Buckingham_Palace": {
+      "rightTop": "buckingham.jpg",
+      "rightBottom": "buckingham-4.jpg",
+      "leftTop": "buckingham-2.jpg",
+      "leftBottom": "buckingham.webp",
+      "rightCarousel": [
+        "buckingham.jpg",
+        "buckingham-aerial.jpg",
+        "buckingham-4.jpg"
+      ],
+      "leftCarousel": [
+        "buckingham-2.jpg",
+        "buckingham.webp",
+        "buckingham-throne.png"
+      ]
+    },
+    "Westminster_Abbey": {
+      "leftBottom": "westminster-abbey-2.jpg",
+      "rightTop": "westminster-abbey-3.jpg",
+      "rightBottom": "westminster-abbey-6.jpg",
+      "leftCarousel": [
+        "westminster-abbey.jpg",
+        "westminster-abbey-2.jpg"
+      ],
+      "rightCarousel": [
+        "westminster-abbey-4.jpg",
+        "westminster-abbey-3.jpg",
+        "westminster-abbey-6.jpg"
+      ]
+    },
+    "Washington_Dulles_International_Airport": {
+      "leftCarousel": [
+        "washington-dulles.jpg"
+      ],
+      "rightCarousel": [
+        "washington-dulles-2.jpg"
+      ]
+    },
+    "London_Eye": {
+      "leftCarousel": [
+        "london-eye-5.jpg"
+      ],
+      "rightCarousel": [
+        "london-eye-3.jpg",
+        "london-eye-4.jpg"
+      ]
+    },
+    "Tate_Modern": {
+      "rightCarousel": [
+        "tate-modern-3.jpg",
+        "tate-modern-6.jpg"
+      ],
+      "leftCarousel": [
+        "tate-modern.jpg",
+        "tate-modern-5.jpg",
+        "tate-modern.webp"
+      ]
+    },
+    "Shakespeare's_Globe": {
+      "leftCarousel": [
+        "globe-theatre.jpg",
+        "globe-theatre-3.jpg"
+      ],
+      "rightCarousel": [
+        "globe-theatre-2.jpg",
+        "globe-theatre-4.jpg"
+      ]
+    },
+    "Tower_Bridge": {
+      "leftCarousel": [
+        "tower-bridge-3.jpg",
+        "tower-bridge-4.jpg"
+      ],
+      "rightCarousel": [
+        "tower-bridge.jpg",
+        "tower-bridge-6.jpg",
+        "tower-bridge-7.jpg"
+      ]
+    },
+    "British_Museum": {
+      "leftCarousel": [
+        "british-museum-2.jpg",
+        "british-museum-3.jpg"
+      ],
+      "rightCarousel": [
+        "british-museum-4.jpg",
+        "british-museum-5.jpg"
+      ]
+    },
+    "National_Gallery": {
+      "leftCarousel": [
+        "national-gallery.jpg",
+        "national-gallery-2.jpg"
+      ],
+      "rightCarousel": [
+        "national-gallery-3.jpg",
+        "national-gallery-4.jpg"
+      ]
+    },
+    "Trafalgar_Square": {
+      "leftCarousel": [
+        "trafalgar-sq.jpg",
+        "trafalgar-sq-3.jpg",
+        "trafalgar-sq.png"
+      ],
+      "rightCarousel": [
+        "trafalgar-sq-4.jpg",
+        "trafalgar-sq-5.jpg"
+      ]
+    },
+    "Leicester_Square": {
+      "leftCarousel": [
+        "london-7.jpg"
+      ],
+      "rightCarousel": []
+    },
+    "Piccadilly_Circus": {
+      "leftCarousel": [],
+      "rightCarousel": []
+    },
+    "Forbidden_Planet_London_Megastore": {
+      "leftCarousel": [],
+      "rightCarousel": []
+    },
+    "John_Snow_cholera_pump": {
+      "leftCarousel": [],
+      "rightCarousel": []
+    },
+    "Soho": {
+      "leftCarousel": [],
+      "rightCarousel": []
+    },
+    "Oxford_Street": {
+      "leftCarousel": [],
+      "rightCarousel": []
+    },
+    "Ye_Olde_Cheshire_Cheese": {
+      "leftCarousel": [],
+      "rightCarousel": []
+    },
+    "Dishoom_Covent_Garden": {
+      "leftCarousel": [],
+      "rightCarousel": []
+    },
+    "Bletchley_Park": {
+      "leftCarousel": [],
+      "rightCarousel": []
+    },
+    "Shakespeare's_Birthplace": {
+      "leftCarousel": [],
+      "rightCarousel": []
+    },
+    "Royal_Shakespeare_Theatre": {
+      "leftCarousel": [],
+      "rightCarousel": []
+    },
+    "Anne_Hathaway's_Cottage": {
+      "leftCarousel": [],
+      "rightCarousel": []
+    },
+    "River_Avon,_Warwickshire": {
+      "leftCarousel": [],
+      "rightCarousel": []
+    },
+    "London": {
+      "leftCarousel": [],
+      "rightCarousel": []
+    },
+    "Rock_and_Sole_Plaice": {
+      "leftCarousel": [],
+      "rightCarousel": []
+    },
+    "LNER": {
+      "leftCarousel": [],
+      "rightCarousel": []
+    },
+    "Edinburgh": {
+      "leftCarousel": [],
+      "rightCarousel": []
+    },
+    "Edinburgh_Castle": {
+      "leftCarousel": [],
+      "rightCarousel": []
+    },
+    "Arthur's_Seat": {
+      "leftCarousel": [],
+      "rightCarousel": []
+    }
+  },
+  "previewImports": {}
+};
